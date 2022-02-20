6ed1293f6a5932078cba18def3130e6f84657f6d
Nitheesh <nitheeshhs@gmail.com>
2022-02-20 17:35:27 +0530

    login change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,9 @@ const Home = () => {
     
   });
 
-
   const loginValidate = async () => 
   {
  
-
 
     const response =await DashboardClient.validateUser(credential)
 
@@ -28,6 +26,7 @@ const Home = () => {
     }
     else{
       console.log("Invalid credentials");
+      alert("Invalid credentials");
     }
    
     
@@ -44,45 +43,50 @@ const setCredentails = async (e) =>
 <>
 
 <section className="vh-100">
-  <div className="container-fluid h-custom" style={{height: "90%"}}>
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid"
-          alt="Sample image"/>
-      </div>
-      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form>
-        
-
-        
-
-          <div className="form-outline mb-4">
-            <input type="email" id="form3Example3" className="form-control form-control-lg"
-              placeholder="Ented a Valid User ID" onChange={(e)=>setCredentails(e)} />
-   
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+        <div class="row flex-grow">
+          <div class="col-lg-6 d-flex align-items-center justify-content-center">
+            <div class="auth-form-transparent text-left p-3">
+              <div class="brand-logo">
+                <img src="C:\Users\nithe\OneDrive\Pictures" alt="logo"/>
+              </div>
+              <h4>Welcome back!</h4>
+              <h6 class="font-weight-light">Happy to see you again!</h6>
+              <form class="pt-3">
+                <div class="form-group">
+                  <label for="exampleInputEmail">Username</label>
+                  <div class="input-group">
+                    <input type="email" id="form3Example3" className="form-control form-control-lg"
+                    placeholder="Ented a Valid User ID" onChange={(e)=>setCredentails(e)} />                 
+                  </div>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input"/>
+                      Keep me signed in
+                    </label>
+                  </div>
+                  <a href="#" class="auth-link text-black">Forgot password?</a>
+                </div>
+                <div class="my-3">
+                       <button type="button" className="btn btn-primary btn-lg"
+                      style={{paddingLeft:"2.5rem", paddingRight: " 2.5rem"}}  onClick={(e) => loginValidate()}>Login</button>              
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Don't have an account? <a href="register-2.html" class="text-primary">Create</a>
+                </div>
+              </form>
+            </div>
           </div>
-
-
-         
-
-    
-          <div className="text-center text-lg-start mt-4 pt-2">
-            <button type="button" className="btn btn-primary btn-lg"
-              style={{paddingLeft:"2.5rem", paddingRight: " 2.5rem"}}  onClick={(e) => loginValidate()}>Login</button>
-         
+          <div class="col-lg-6 login-half-bg d-flex flex-row">
+            <p class="text-white font-weight-medium text-center flex-grow align-self-end">Copyright &copy; 2021  All rights reserved.</p>
           </div>
-
-        </form>
+        </div>
       </div>
     </div>
-  </div>
-  <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-
-    <div className="text-white mb-3 mb-md-0">
-      Copyright © 2020. All rights reserved.
-    </div>
-
-   
   </div>
 </section>
 
@@ -91,3 +95,6 @@ const setCredentails = async (e) =>
 };
   
 export default Home;
+
+
+
